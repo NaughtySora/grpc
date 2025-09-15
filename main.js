54aa4cb62c78c5761bc7145be0ebf09ecf3e660f
Trofymenko Vladyslav <naughtysora@proton.me>
2025-09-15 19:40:50 +0300

    types

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const client = require("./lib/client/index.js");
+const server = require("./lib/server/index.js");
+const utils = require("./lib/util/index.js");
+
+module.exports = {
+  ...utils,
+  client,
+  server
+};
